8040efa84dc516a716aace9acd4963c360c612a8
yishuai <chenyishuai@gmail.com>
2021-05-04 09:39:46 +0800

    u

---
+++ test/0-w3/0-flow.js
@@ -0,0 +1,85 @@
+// 1. Write a JavaScript program that accept two integers and display the larger.
+
+function display_larger_integers(x, y) {
+
+}
+
+console.log(display_larger_integers(25, 35));
+
+// 2. Write a JavaScript conditional statement to find the sign of product of three numbers. Display an alert box with the specified sign.
+
+function find_numbers_product_sign(x, y, z) {
+
+}
+
+console.log(find_numbers_product_sign(3, -7, 2));
+
+// 3. Write a JavaScript conditional statement to sort three numbers. Display an alert box to show the result.
+
+function numbers_sort(x, y, z) {}
+
+console.log(numbers_sort(0, -1, 4));
+
+// 4. Write a JavaScript conditional statement to find the largest of five numbers. Display an alert box to show the result.
+
+function find_the_largest_numbers(a, b, c, d, e) {}
+
+console.log(find_the_largest_numbers(-5, -2, -6, 0, -1));
+
+// 5. Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen.
+
+function check_whether_the_numbers_is_odd_or_even() {
+
+}
+
+check_whether_the_numbers_is_odd_or_even();
+
+// 6. Write a JavaScript program which compute, the average marks of the following students Then, this average is used to determine the corresponding grade.
+
+function compute_the_average_of_students() {
+    let name_and_score = [["David", 80], ["Vinoth", 77], ["Divya", 88], ["Ishitha", 95], ["Thomas", 68]];
+
+}
+
+compute_the_average_of_students();
+
+// 7. Write a JavaScript program which iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+
+function print_multiples_of_three_or_five() {
+
+}
+
+print_multiples_of_three_or_five();
+
+// 8. Write a JavaScript program to find and print the first 5 happy numbers.
+
+function find_and_print_the_top_five_happy_numbers() {
+    // 创建用于存放快乐数字的数组
+}
+
+console.log(find_and_print_the_top_five_happy_numbers());
+
+// 9. Write a JavaScript program to find the armstrong numbers of 3 digits.
+
+function find_armstrong_number() {}
+
+find_armstrong_number();
+
+// 10. Write a JavaScript program to construct the following pattern, using a nested for loop.
+// *
+// **
+// ***
+// ****
+// *****
+
+// 11. Write a JavaScript program to compute the greatest common divisor (GCD) of two positive integers.
+
+function common_factors() {}
+
+console.log(common_factors());
+
+// 12. Write a JavaScript program to sum the multiples of 3 and 5 under 1000.
+
+function sum_of_multiples() {}
+
+console.log(sum_of_multiples());
---
+++ test/0-w3/1-math-1.js
@@ -0,0 +1,65 @@
+// 1. Write a JavaScript function to convert a number from one base to another
+
+function base_conversion(number, initial_base, change_base) {
+
+}
+console.log(base_conversion(1000, 2, 8));
+
+
+// 2. Write a JavaScript function to convert a binary number to a decimal number
+
+function binary_conversion(binary_string) {
+
+}
+console.log(binary_conversion("110011"));
+
+
+// 3. Write a JavaScript function to convert a decimal number to binary, hexadecimal or octal number
+
+function decimal_conversion(decimal_string, hexadecimal_character) {
+
+}
+console.log(decimal_conversion(120, 'B'));
+
+
+// 4. Write a JavaScript function to generate a random integer
+
+function random_number(number, number2) {}
+
+console.log(random_number(20, 1));
+
+// 5. Write a JavaScript function to format a number up to specified decimal places
+
+function keep_decimal_position(decimal, position) {}
+
+console.log(keep_decimal_position(2.100212, 2));
+
+// 6. Write a JavaScript function to find the highest value in an array
+
+function find_array_maximum(array) {}
+
+console.log(find_array_maximum([12, 34, 56, 1]));
+
+// 7. Write a JavaScript function to find the lowest value in an array
+
+function find_array_minimum(array) {}
+
+console.log(find_array_minimum([12, 34, 56, 1]));
+
+// 8. Write a JavaScript function to get the greatest common divisor (gcd) of two integers
+
+function get_gcd(number, number_2) {}
+
+console.log(get_gcd(9, 3));
+
+// 9. Write a JavaScript function to find the GCD (greatest common divisor) of more than 2 integers
+
+function find_multiple_integers_gcd(array) {}
+
+console.log(find_multiple_integers_gcd([3, 15, 27]));
+
+// 10. Write a JavaScript function to get the least common multiple (LCM) of two numbers
+
+function get_lcm(number, number_2) {}
+
+console.log(get_lcm(3, 15));
